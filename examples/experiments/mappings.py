987787df125ad6c62ecaec529bdc12fa482bbdb7
Charles <xuc@berkeley.edu>
2024-10-30 09:46:36 -0700

    added mappings file

---
+++ examples/experiments/mappings.py
@@ -0,0 +1,11 @@
+from experiments.ram_insertion.config import TrainConfig as RAMInsertionTrainConfig
+from experiments.usb_pickup_insertion.config import TrainConfig as USBPickupInsertionTrainConfig
+from experiments.object_handover.config import TrainConfig as ObjectHandoverTrainConfig
+from experiments.egg_flip.config import TrainConfig as EggFlipTrainConfig
+
+CONFIG_MAPPING = {
+                "ram_insertion": RAMInsertionTrainConfig,
+                "usb_pickup_insertion": USBPickupInsertionTrainConfig,
+                "object_handover": ObjectHandoverTrainConfig,
+                "egg_flip": EggFlipTrainConfig,
+               }
