013b1f31a00b23ac896f5452c4eed4e94f42cf90
Jeffrey Wu <jeffrey.wu13579@berkeley.edu>
2024-11-06 00:17:32 -0500

    fix exp name typo

--- examples/experiments/ram_insertion/run_learner.sh
+++ examples/experiments/ram_insertion/run_learner.sh
@@ -1,7 +1,7 @@
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.3 && \
 python ../../train_rlpd.py "$@" \
-    --exp_name=ram_insert \
+    --exp_name=ram_insertion \
     --checkpoint_path=../../experiments/ram_insertion/debug \
     --demo_path=... \
     --learner \
