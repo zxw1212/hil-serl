e2e2b46129636983fb85d438e686e5ca64d5c7af
Charles <xuc@berkeley.edu>
2024-11-05 22:07:20 -0800

    not save frames by default to save memory

--- examples/experiments/egg_flip/run_actor.sh
+++ examples/experiments/egg_flip/run_actor.sh
@@ -2,5 +2,5 @@ export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.1 && \
 python ../../train_rlpd.py "$@" \
     --exp_name=egg_flip \
-    --checkpoint_path=../../experiments/egg_flip/debug \
+    --checkpoint_path=first_run \
     --actor \
--- examples/experiments/ram_insertion/run_actor.sh
+++ examples/experiments/ram_insertion/run_actor.sh
@@ -2,5 +2,5 @@ export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.1 && \
 python ../../train_rlpd.py "$@" \
     --exp_name=ram_insertion \
-    --checkpoint_path=../../experiments/ram_insertion/debug \
+    --checkpoint_path=first_run \
     --actor \
