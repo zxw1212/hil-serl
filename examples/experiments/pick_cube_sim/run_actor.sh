963552e51cdd759f4691cfe90d4e061bf056925b
s1lent4gnt <kmeftah.khalil@gmail.com>
2025-01-22 20:31:14 +0100

    edit script args

--- examples/experiments/pick_cube_sim/run_actor.sh
+++ examples/experiments/pick_cube_sim/run_actor.sh
@@ -2,5 +2,5 @@ export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.1 && \
 python ../../train_rlpd_sim.py "$@" \
     --exp_name=pick_cube_sim \
-    --checkpoint_path=second_run \
+    --checkpoint_path=six_run \
     --actor \
