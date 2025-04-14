6461b1b3f71f1775bc34de4e26e323aadd7048fa
zxw1212 <1419739886@qq.com>
2025-04-14 15:20:43 +0800

    delete the ckpt

--- examples/experiments/astribot_test/run_learner.sh
+++ examples/experiments/astribot_test/run_learner.sh
@@ -3,5 +3,5 @@ export XLA_PYTHON_CLIENT_MEM_FRACTION=.8 && \
 python ../../train_rlpd.py "$@" \
     --exp_name=astribot_test \
     --checkpoint_path=../../experiments/astribot_test/test1 \
-    --demo_path=../../experiments/astribot_test/demo_data/astribot_test_10_demos_2025-04-11_14-37-06.pkl \
+    --demo_path=../../experiments/astribot_test/demo_data/astribot_test_10_demos_2025-04-14_12-08-25.pkl \
     --learner \
